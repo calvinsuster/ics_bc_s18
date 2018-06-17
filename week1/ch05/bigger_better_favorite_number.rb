31b1a72db6611abea0b51a414669a9062952ce51
calvinsuster <calvinsuster@berkeley.edu>
2018-06-17 14:20:57 -0700

    Complete

---
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -0,0 +1,5 @@
+puts 'Hello, what is your favorite number?'
+number = gets.chomp
+var = 1
+new_number = puts number.to_i + var 
+puts 'I believe this may be a better number' + ' ' + new_number.to_s 
---
+++ week1/ch05/full_name_greeting.rb
@@ -0,0 +1,7 @@
+puts 'Hello what\'s your first name?' 
+f_name = gets.chomp 
+puts 'Middle name?'
+m_name = gets.chomp
+puts 'Please enter your last name.'
+l_name = gets.chomp
+puts 'Your name is ' + f_name + ' ' + m_name + ' ' + l_name + '!'
